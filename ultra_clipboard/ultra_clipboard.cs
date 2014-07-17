b4471245d21b5f7f1645179048ef5b9b24123b4f
Anubhav410 <shrianu410@gmail.com>
2014-07-17 16:45:16 +0530

    Starting Project

---
+++ ultra_clipboard/ultra_clipboard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+public class ultra_clipboard : System.ServiceProcess.ServiceBase  
+{
+       public ultra_clipboard() 
+	{
+		this.ServiceName = "ultra_clipboard";
+		this.CanStop = true;
+		this.CanPauseAndContinue = true;
+		this.AutoLog = true;
+	}
+
+       public static void Main()
+       {
+           System.ServiceProcess.ServiceBase.Run(new ultra_clipboard());
+       }
+
+       protected override void OnStart(string[] args)
+       {
+           string lines = "First line.\r\nSecond line.\r\nThird line.";
+
+           // Write the string to a file.
+           System.IO.StreamWriter file = new System.IO.StreamWriter("c:\\test.txt");
+           file.WriteLine(lines);
+
+           file.Close();
+       }
+}
+
+ 
